0a7f889e6b2f008bdd8f5d6365cf81e0c93368de
shezafatima <sohailejaz058@gmail.com>
2025-08-12 17:20:27 +0500

    logo changes

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -3,16 +3,16 @@ import Link from "next/link";
 
 export default function Logo() {
   return (
-    <Link href={"/"} className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0 ">
+    <Link href={"/"} className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0 gap-2">
       <Image
-        src="/images/MSE_logo.png"
+        src="/images/hp-logo.png"
         loading="lazy"
         alt="MSE Enterprises Logo"
         className="w-24 "
         width={400}
         height={400}
       />
-       <p className="text-[9px] text-blue-600 font-bold -ml-3 mt-3 capitalize">Haris Packages</p>
+       <p className="text-[11px] text-blue-600 font-bold -ml-3 mt-3 capitalize ">Haris Packages</p>
     </Link>
   );
 }
